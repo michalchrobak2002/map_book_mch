2bb9a56a860eaa66d6e56cfe27c2e646989e86e5
michalchrobak2002 <mchrobak2002@gmail.com>
2025-04-09 13:09:08 +0200

    wip: data structure development mvc

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-users: list = [
-    {'name': 'Julia', 'location': 'Ząbki', 'posts': 10},
-    {'name': 'Julia', 'location': 'Sokółka', 'posts': 20},
-    {'name': 'Klaudia', 'location': 'Warszawa', 'posts': 15},
-    {'name': 'Marcin', 'location': 'Grudziądz', 'posts': 1000},
-    {'name': 'Mateusz', 'location': 'Lublin', 'posts': 100},
-]
+from utils.model import users
+from utils.controller import get_user_info
+
+def main():
+    print(f'Witaj {users[0]['name']}')
+    get_user_info(users[1:])
 
 
 
-
-
-def get_user_info(users_data: list)->None:
-    for user in users_data:
-        print(f'Twój znajomy {user['name']} z miejscowości {user['location']} opublikował {user["posts"]}')
-
-print(f'Witaj {users[0]['name']}')
-get_user_info(users[1:])
-
-
+if __name__ == '__main__':
+    main()
